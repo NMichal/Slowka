7842a2aa195e464ee73a5317a430795ba895fa2c
Michal N <michal.niewolik@gmail.com>
2017-04-27 22:10:37 +0200

    Dodanie etykiet liczby literek oraz możliwość wyświetlania na etykiecie int

--- Slowka/Main.cpp
+++ Slowka/Main.cpp
@@ -1,7 +1,11 @@
 #include <iostream> 
 #include <windows.h>
+#include <string> 
 
 using namespace std;
+
+#define ID_ETYKIETA_LICZBA_LITEREK 10
+
 
 
 ///mo¿emy sparsowaæ liniê œcie¿ki ¿eby dodaæ coœ ciekawego np. jakieœ cheaty - u³atwienia
@@ -13,6 +17,9 @@ using namespace std;
 //pass - gdy nie mamy nic (chocia¿ mo¿e lepiej tylko wymieñ literki)
 // Jakieœ komunikaty
 // Licznik ile pozosta³o literek w "worku"
+// lowercase - przy pobieraniu z textboxa (sprawdziæ czy wszystko w s³owniku bêdzie ma³e)
+
+// byæ mo¿e póŸniej dodaæ ID_PRZYCISKOW 
 
 
 
@@ -21,8 +28,13 @@ MSG Komunikat;
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
+#pragma region
 HWND buttonWymienLiterki;
 HWND buttonZatwierdz;
+HWND textBoxWpisaneSlowo;
+HWND staticTextEtykietaLiczbaLiterek;
+HWND staticTextLiczbaLiterek;
+#pragma endregion Deklaracja kontrolek okna
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
@@ -52,12 +64,12 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	}
 
 	// TWORZENIE OKNA
-	HWND hwnd;
+	HWND OknoAplikacji;
 
-	hwnd = CreateWindowEx(WS_EX_CLIENTEDGE, NazwaKlasy, "Oto okienko", WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT, CW_USEDEFAULT, 1200, 700, NULL, NULL, hInstance, NULL);
+	OknoAplikacji = CreateWindowEx(WS_EX_CLIENTEDGE, NazwaKlasy, "Oto okienko", WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, 750, 700, NULL, NULL, hInstance, NULL);
 
-	if (hwnd == NULL)
+	if (OknoAplikacji == NULL)
 	{
 		MessageBox(NULL, "Okno odmówi³o przyjœcia na œwiat!", "Ale kicha...", MB_ICONEXCLAMATION);
 		return 1;
@@ -65,15 +77,28 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 #pragma region
 	buttonWymienLiterki = CreateWindowEx(0, "BUTTON", "Wymieñ literki", WS_CHILD | WS_VISIBLE,
-		1000, 600, 150, 30, hwnd, NULL, hInstance, NULL); //hwnd - nasz uchyt okna g³ównego
+		550, 600, 150, 30, OknoAplikacji, NULL, hInstance, NULL); //hwnd - nasz uchyt okna g³ównego
 
 	buttonZatwierdz = CreateWindowEx(0, "BUTTON", "Zatwierdz", WS_CHILD | WS_VISIBLE,
-		800, 600, 150, 30, hwnd, NULL, hInstance, NULL); //hwnd - nasz uchyt okna g³ównego
+		350, 600, 150, 30, OknoAplikacji, NULL, hInstance, NULL); //hwnd - nasz uchyt okna g³ównego
 
-#pragma endregion Inicjalizacja przyciskow
+	textBoxWpisaneSlowo = CreateWindowEx(WS_EX_CLIENTEDGE, "EDIT", NULL, WS_CHILD | WS_VISIBLE | WS_BORDER,
+		50, 600, 250, 30, OknoAplikacji, NULL, hInstance, NULL);
 
-	ShowWindow(hwnd, nCmdShow); // Poka¿ okienko...
-	UpdateWindow(hwnd);
+	staticTextEtykietaLiczbaLiterek = CreateWindowEx(0, "STATIC", NULL, WS_CHILD | WS_VISIBLE |
+		SS_LEFT, 600, 50, 150, 30, OknoAplikacji, NULL, hInstance, NULL);
+	SetWindowText(staticTextEtykietaLiczbaLiterek, "Liczba pozosta³ych \n\tliterek:");
+
+	staticTextLiczbaLiterek = CreateWindowEx(0, "STATIC", NULL, WS_CHILD | WS_VISIBLE |
+		SS_CENTER, 600, 80, 150, 30, OknoAplikacji, (HMENU)ID_ETYKIETA_LICZBA_LITEREK, hInstance, NULL);
+
+	int i = 10;
+	SetDlgItemInt(OknoAplikacji, ID_ETYKIETA_LICZBA_LITEREK, i, true);	//---!!!---Ustwaienie etyliety liczby literek z inta ---!!!---
+
+#pragma endregion Inicjalizacja kontrolek okna
+
+	ShowWindow(OknoAplikacji, nCmdShow); // Poka¿ okienko...
+	UpdateWindow(OknoAplikacji);
 
 	// Pêtla komunikatów - klikniêcie myszk¹, wciœniêcie klawisza czy zamkniêcie okna
 	while (GetMessage(&Komunikat, NULL, 0, 0)) // Gdy robimy jak¹œ czynnoœæ, która wi¹¿e siê z powstaniem jakiejœ wiadomoœci, np. poruszamy mysz¹, wiadomoœæ posy³ana jest do kolejki wiadomoœci, sk¹d zabiera j¹ GetMessage. 
